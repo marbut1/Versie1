66eb2aa9bf4b2fffbaba266ec994f6155cf6b4bc
marbut1 <71845014+marbut1@users.noreply.github.com>
2024-04-24 16:30:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//Eerste versie: basis
+
+import React from "react";
+const title = "React";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App;
